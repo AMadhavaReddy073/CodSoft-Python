34d351a36bc8c8cb3a397b2d70b142f972a9810f
AMadhavaReddy073 <amadhav573@gmail.com>
2024-09-03 01:50:30 +0530

    Update Calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -4,7 +4,6 @@ def add (x, y):
 def multiply (x, y):
     return x * y
 
-
 def divide (x, y):
     return (x / y)
 
@@ -14,6 +13,7 @@ def subtract (x, y):
 def modulus (x, y):
     return (x // y)
 
+#displays avaliable arithmetic operations to user
 print("Select operation.")
 print("1.Add")
 print("2.Multiply")
@@ -22,29 +22,35 @@ print("4.Subtract")
 print("5.Modulus")
 
 while True:
+    #user selects on option to perform
     user_Input = input("Select the operation 1/2/3/4/5: ")
 
     if user_Input in ("1", "2", "3", "4", "5"):
         try:
+            #taking two number as input for operations
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
         except ValueError:
             print("Invalid input. Please enter a number.")
             continue
         
-
+        #addition
         if user_Input == '1':
             print(num1, "+", num2, "=", add(num1, num2))
 
+        #multiplication
         elif user_Input == '2':
             print(num1, "*", num2, "=", multiply(num1, num2))
 
+        #division
         elif user_Input == '3':
             print(num1, "/", num2, "=", divide(num1, num2))
 
+        #minus
         elif user_Input == '4':
             print(num1, "-", num2, "=", subtract(num1, num2))
         
+        #modulus
         elif user_Input == "5":
             print(num1, "//", num2, "=", modulus(num1, num2))
 
